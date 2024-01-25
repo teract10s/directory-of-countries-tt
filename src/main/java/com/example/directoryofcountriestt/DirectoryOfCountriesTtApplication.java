9b66383ca83d647047d38ddd5c30a38f3c9f8672
Teract10s <trs.leonov@gmail.com>
2024-01-25 23:19:39 +0200

    init

---
+++ src/main/java/com/example/directoryofcountriestt/DirectoryOfCountriesTtApplication.java
@@ -0,0 +1,13 @@
+package com.example.directoryofcountriestt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DirectoryOfCountriesTtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DirectoryOfCountriesTtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/directoryofcountriestt/DirectoryOfCountriesTtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.directoryofcountriestt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DirectoryOfCountriesTtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
