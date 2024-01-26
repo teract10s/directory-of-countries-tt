d6d84203520f412ec9539f28cd1e1d091bff1811
Teract10s <trs.leonov@gmail.com>
2024-01-26 20:05:38 +0200

    added README

--- src/main/java/com/example/directoryofcountriestt/controller/CountryController.java
+++ src/main/java/com/example/directoryofcountriestt/controller/CountryController.java
@@ -2,6 +2,8 @@ package com.example.directoryofcountriestt.controller;
 
 import com.example.directoryofcountriestt.dto.CountryResponseDto;
 import com.example.directoryofcountriestt.service.CountryService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,10 +12,16 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/countries")
+@Tag(name = "Country controller",
+        description = "Here we have endpoint for getting information about country")
 public class CountryController {
     @Autowired
     private CountryService countryService;
 
+    @Operation(summary = "Get info about country",
+            description = "As a user you have to give country name(in cyrillic or latin) or country code." +
+                    "As a request you will get NOT_FOUND if we don't have info about this country" +
+                    " or json about if we have information about the country.")
     @GetMapping
         public CountryResponseDto getInfoByCountryByName(@RequestParam(required = false) String countryName,
                                                          @RequestParam(required = false) Long countryCode) {
